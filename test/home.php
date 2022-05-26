df7c9f4c746afe551db4a1c46071e0fe6a5d34c7
rostand kinfack <kinfackrostand@gmail.com>
2022-05-26 21:17:39 +0200

    ajout de condition isset

--- test/home.php
+++ test/home.php
@@ -7,11 +7,22 @@
     <body>
         <h1>Validation de vos coordonnées!</h1>
         <div>
+            <?php
+                if( !isset($_GET['nom']) ||!isset($_GET['prenom']) || !isset($_GET['email']) || !isset($_GET['age'])
+                )
+                { 
+
+                echo "veillez rentrer vos coordonnées s'isl vous plaît";
+                return;
+                }
+                ?>
+            
             <h2>rappel de vos données </h2>
-            <p><span>votre nom:</span><?php echo $_GET['nom'];?></p>
-            <p><span>votre prénom:</span><?php echo $_GET['prenom'];?></p>
-            <p><span>votre email:</span><?php echo $_GET['email'];?></p>
-            <p><span>vous avez:</span><?php echo $_GET['age'] .'ans';?></p>
+            <p><span>votre nom</span>:<?php echo $_GET['nom'];?></p>
+            <p><span>votre prénom</span>:<?php echo $_GET['prenom'];?></p>
+            <p><span>votre email</span>:<?php echo $_GET['email'];?></p>
+            <p><span>vous avez</span>:<?php echo $_GET['age'] .'ans';?></p>
+        
         </div>
 </body>
 </html>
